08be614c08941b62b6da33b6d07003e84147e3d0
EnjoMitch <ender-sz@go2.pl>
2021-06-15 10:53:01 +0200

    Update ci-gcc.sh

--- ci-gcc.sh
+++ ci-gcc.sh
@@ -5,5 +5,6 @@ GEN="CodeBlocks - Unix Makefiles"
 
 mkdir -p build && cd build
 cmake -G "${GEN}" ..\
-&& ${MAKE} -j `nproc` || ${MAKE} && ctest --verbose -R EnjoLibTest || ctest --output-on-failure -j `nproc` && ${MAKE} -j `nproc` install
+&& ${MAKE} -j `nproc` || ${MAKE} && ctest --verbose -R EnjoLibTest 
+ctest --output-on-failure -j `nproc` && ${MAKE} -j `nproc` install
 
