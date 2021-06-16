b69cff571d7d5e17e63c3c0f131de04382d60748
mj-xmr <mjxmr@protonmail.com>
2021-06-16 19:17:13 +0200

    ci-mingw

--- ci-gcc.sh
+++ ci-gcc.sh
@@ -2,9 +2,11 @@
 
 MAKE=make
 GEN="CodeBlocks - Unix Makefiles"
+BUILD_DIR="build/gcc"
 
-mkdir -p build && cd build
-cmake -G "${GEN}" ..\
+mkdir -p $BUILD_DIR && cd $BUILD_DIR
+
+cmake -G "${GEN}" ../.. \
 && ${MAKE} -j `nproc` || ${MAKE} && ctest --verbose -R EnjoLibTest 
 ctest --output-on-failure -j `nproc` && ${MAKE} -j `nproc` install
 
---
+++ ci-mingw.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MAKE=make
+GEN="MSYS Makefiles"
+
+GCC_PREFIX=x86_64-w64-mingw32
+CMAKE_C_COMPILER="${GCC_PREFIX}-gcc"
+CMAKE_CXX_COMPILER="${GCC_PREFIX}-g++"
+
+BUILD_DIR="build/mingw"
+
+mkdir -p $BUILD_DIR && cd $BUILD_DIR
+cmake -G "${GEN}" ../.. \
+-DCMAKE_C_COMPILER="${CMAKE_C_COMPILER}" \
+-DCMAKE_CXX_COMPILER="${CMAKE_CXX_COMPILER}" \
+&& ${MAKE} -j `nproc` || ${MAKE} && ctest --verbose -R EnjoLibTest 
+ctest --output-on-failure -j `nproc` && ${MAKE} -j `nproc` install
+
