e3ddb5595f131b9e4ad0a59e83dd62865a3ea45e
hajerchebil <hajourasoulayma@gmail.com>
2021-11-28 15:40:02 -0500

    try to fix

--- lib/Util/TimerChrono.cpp
+++ lib/Util/TimerChrono.cpp
@@ -45,20 +45,20 @@ TimerChrono::~TimerChrono()
 double TimerChrono::ToNowSeconds() const
 {
     auto tend = Clock::now();
-    return std::chrono::duration_cast<std::chrono::seconds>((long)tend - (long)m_start).count();
+    return std::chrono::duration_cast<std::chrono::seconds>(static_cast<long>tend - static_cast<long>m_start).count();
 }
 double TimerChrono::ToNowMilliseconds() const
 {
     auto tend = Clock::now();
-    return std::chrono::duration_cast<std::chrono::milliseconds>((long)tend - (long)m_start).count();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(static_cast<long>tend - static_cast<long>m_start).count();
 }
 double TimerChrono::ToNowMicroseconds() const
 {
     auto tend = Clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>((long)tend - (long)m_start).count();
+    return std::chrono::duration_cast<std::chrono::microseconds>(static_cast<long>tend - static_cast<long>m_start).count();
 }
 long TimerChrono::ToNowNanoseconds() const
 {
     auto tend = Clock::now();
-    return std::chrono::duration_cast<std::chrono::nanoseconds>((long)tend - (long)m_start).count();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(static_cast<long>tend - static_cast<long>m_start).count();
 }
