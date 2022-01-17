020bc4418e4c2601e0fc5bf2e302f21b7d43673e
EnjoMitch <ender-sz@go2.pl>
2022-01-17 21:35:08 +0100

    UI utility functions: FindFileFillTxt & ExecuteDefaultEditorOnFile (#30)
    Co-authored-by: EnjoMitch <EnjoMitch@users.noreply.github.com>

--- libwx/src/UtilWx.cpp
+++ libwx/src/UtilWx.cpp
@@ -1,7 +1,10 @@
 #include "pch/wx_pch.h"
 #include "UtilWx.hpp"
 
+#include <Template/SafePtr.hpp>
+
 #include <wx/stdpaths.h>
+#include <wx/mimetype.h>
 
 using namespace EnjoLib;
 
@@ -19,4 +22,39 @@ wxString UtilWx::GetHomeDir()
 bool UtilWx::IsLinux()
 {
     return (wxGetOsVersion() == wxOS_UNIX_LINUX);
+}
+
+bool UtilWx::FindFileFillTxt(wxWindow * parent, wxTextCtrl * ctrl)
+{
+    const wxString & fil = ctrl->GetValue();
+
+    wxFileDialog openFileDialog(parent, _("Find " + fil), "", fil,  "", wxFD_PREVIEW);
+    if (openFileDialog.ShowModal() == wxID_CANCEL)
+        return false;     // the user changed idea...
+
+    ctrl->SetValue(openFileDialog.GetPath());
+    
+    return true;
+}
+
+bool UtilWx::ExecuteDefaultEditorOnFile(const wxString & filePath, const wxString & extension)
+{
+    // 1) Get File Type
+    EnjoLib::SafePtr<wxFileType> c_type(wxTheMimeTypesManager->GetFileTypeFromExtension(extension));
+    if(!c_type.IsValid())
+    {
+        wxMessageBox("Could not find association");
+        return false;
+    }
+
+    // 2) Get Open Message
+    const wxString command = c_type->GetOpenCommand(filePath);
+    if(!command)
+    {
+        wxMessageBox("No default program");
+        return false; //No defoult program
+    }
+    // 3) Execute message
+    wxExecute(command);
+    return true;
 }
