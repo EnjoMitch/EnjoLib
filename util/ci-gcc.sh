5c0689fa8b4003115921118b6dbc6168e8b47da6
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-08-07 08:26:16 +0200

    Low prec test

--- util/ci-gcc.sh
+++ util/ci-gcc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+OPTS=$1
 MAKE=make
 GEN="CodeBlocks - Unix Makefiles"
 BUILD_DIR="build/gcc"
@@ -12,7 +13,7 @@ mkdir -p $BUILD_DIR && cd $BUILD_DIR
 
 cmake -G "${GEN}" ../.. \
  \
- \
+${OPTS} \
 -DUSE_STATIC="OFF" \
 -DUSE_UNITY="ON" \
 -DBUILD_BOOST="ON" \
